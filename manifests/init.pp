5f1e470a9f61f83052e53b8acc9f4f7016900de5
Zack Smith <acidprime@wallcity.org>
2021-02-07 17:06:04 -0800

    Remove old validation functions
    Prior to this commit we had the old pre type validate functions, this commit removes them.

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,42 +33,5 @@ class aptly (
   $manage_xz_utils      = $aptly::params::manage_xz_utils,
 ) inherits aptly::params {
 
-  validate_string(
-    $version,
-    $user,
-    $group,
-    $repo_location,
-    $repo_release,
-    $repo_repos,
-    $repo_keyserver,
-    $config_filepath,
-    $user,
-    $group,
-    $root_dir)
-  if ! is_integer($uid) { fail("invalid ${uid} provided") }
-  if ! is_integer($gid) { fail("invalid ${gid} provided") }
-  validate_re($repo_key, '^[A-F0-9]+$')
-  validate_bool(
-    $install_repo,
-    $enable_service,
-    $enable_api,
-    $api_nolock)
-  validate_array($architectures)
-  validate_hash($properties)
-  validate_hash($s3_publish_endpoints)
-  validate_hash($swift_publish_endpoints)
-  validate_integer($port, 49150)
-  validate_integer($api_port, 49150)
-  validate_re(
-    $bind,
-    '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$',
-    'Aptly Bind IP address is not correct'
-  )
-  validate_re(
-    $api_bind,
-    '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$',
-    'API Bind IP address is not correct'
-  )
-
   class { '::aptly::install': } -> class { '::aptly::config':  } ~> class { '::aptly::service': } -> Class['::aptly']
 }
